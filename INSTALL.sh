2255c87953caf6dd52bb83d3d2895353c4c0023d
Cam <cameronmcmenamie@gmail.com>
2021-09-20 00:53:01 +1000

    add chmod to install.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,8 +1,12 @@
 #! /usr/bin/env bash
+filename="evolve.jl"
 
 # install julia TODO
+# Using julia version 1.6.2 
 
+
+
+chmod +x "$filename"
 
 # install FASTX package
-
 julia -e 'using Pkg; Pkg.add("FASTX")'
