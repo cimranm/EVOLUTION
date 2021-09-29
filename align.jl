8244b6da870e27552e368fceff70a62623b2badd
Cam <cameronmcmenamie@gmail.com>
2021-09-29 20:25:08 +1000

    Add align.jl

---
+++ align.jl
@@ -0,0 +1,14 @@
+#! /usr/bin/env julia
+"""
+# ALIGNMENT
+# implementation of Global Alignment algorithm 
+# -----------------------------------------------------
+
+# usage:  align < INFILE > OUTFILE 
+
+julia version 1.6.2
+
+Written by Cam McMenamie
+"""
+
+using FASTX     # BioJulia / FASTX.jl       https://biojulia.net 
